e480b3ccdff05b97e7ff8b04f5cf786271610d7d
DialecticalLaw <d.shmuratkin@mail.ru>
2023-09-03 21:03:44 +0300

    feat: add basic page layout (Sun, Sep 03, 2023 21:11:45)

---
+++ cssMemeSlider/index.js
@@ -0,0 +1,7 @@
+const paginationBox = document.querySelector('.pagination-box');
+const imgMeme = document.querySelector('.img-meme');
+const sliderText = document.querySelector('.slider-text');
+const button1 = document.querySelector('.button1');
+const button2 = document.querySelector('.button2');
+const button3 = document.querySelector('.button3');
+const button4 = document.querySelector('.button4');
